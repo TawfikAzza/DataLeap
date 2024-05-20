c08c8516457d75be0237f577f84f1cd957a0823b
Tawfik <90477695+TawfikAzza@users.noreply.github.com>
2024-05-20 12:00:07 +0200

    Initial commit

---
+++ MigrationService/Controllers/MigrationServiceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MigrationService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MigrationServiceController : ControllerBase
+{
+    public MigrationServiceController()
+    {
+        
+    }
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello from MigrationService!";
+    }
+}
